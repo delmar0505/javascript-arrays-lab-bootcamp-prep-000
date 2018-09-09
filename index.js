8279ea1d8ed8c7cf1f6553f1a95df6636af20d6e
oscar leon <first.liberal.thinker@gmail.com>
2018-09-09 16:27:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function destructivelyRemoveFirstKitten(){
   return kittens;
 }
 function appendKitten (name){
-  
+    var newKittens = [...kittens]
+  newKittens.push(name)
+  return newKittens
 }
 
